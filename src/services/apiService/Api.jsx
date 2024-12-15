b7f4c0c42fe4700107730922859b47026133a186
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2024-12-15 13:21:33 +0100

    Add userData loading with initial authorization

--- src/services/apiService/Api.jsx
+++ src/services/apiService/Api.jsx
@@ -2,29 +2,58 @@ import { API_URL } from "../../config/config";
 
 export const getTokenFromApi = async (email, password) => {
   try {
-    const payload = {
-      email: email,
-      password: password,
-    };
+    const tokenValue = await firstCall(email, password);
+    const userData = await secondCall(tokenValue);
 
-    const response = await fetch(API_URL + "/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
-
-    if (!response.ok) {
-      throw new Error("Error en la solicitud");
-    }
-
-    const data = await response.json();
-    const tokenValue = data["access_token"];
-    localStorage.setItem("token", tokenValue);
-    return tokenValue;
+    console.log("Datos del usuario:", userData);
+    return { token: tokenValue, userData };
   } catch (error) {
-    console.error("Error al obtener el token de la API:", error.message);
+    console.error("Error al obtener el token o los datos del usuario:", error.message);
     return null;
   }
+};
+
+// Función para obtener el token
+const firstCall = async (email, password) => {
+  const payload = {
+    email: email,
+    password: password,
+  };
+
+  const response = await fetch(API_URL + "/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error en la solicitud para obtener el token");
+  }
+
+  const data = await response.json();
+  const tokenValue = data["access_token"];
+  localStorage.setItem("token", tokenValue);
+
+  return tokenValue;
+};
+
+// Función para obtener los datos del usuario
+const secondCall = async (tokenValue) => {
+  const userResponse = await fetch(API_URL + "/auth/me", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${tokenValue}`,
+    },
+  });
+
+  if (!userResponse.ok) {
+    throw new Error("Error al obtener los datos del usuario");
+  }
+
+  const userData = await userResponse.json();
+  localStorage.setItem("userId", userData["id"]);
+
+  return userData;
 };
